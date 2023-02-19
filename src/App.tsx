0f384cc09b241ddb407c5a85c86b3d53f9bdda91
AlexanderPonarin <114759942+AlexanderPonarin@users.noreply.github.com>
2023-02-19 23:41:50 +0300

    start setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
-import {Route, Routes} from 'react-router-dom'
-import {ProductsPage} from './pages/ProductsPage'
-import {AboutPage} from './pages/AboutPage'
-import {Navigation} from './components/Navigation'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={ <ProductsPage /> } />
-        <Route path="/about" element={ <AboutPage /> } />
-      </Routes>
-    </>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
